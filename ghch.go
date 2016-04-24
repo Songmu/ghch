06a1c7c5b47dd81a5e245634a8180413755813f3
Songmu <y.songmu@gmail.com>
2016-04-25 01:23:55 +0900

    MergedPRNums

--- ghch.go
+++ ghch.go
@@ -2,6 +2,7 @@ package ghch
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
 	"regexp"
@@ -32,11 +33,11 @@ var verReg = regexp.MustCompile(`^v?[0-9]+(?:\.[0-9]+){0,2}$`)
 func (gh *Ghch) Versions() []string {
 	out, _ := gh.Cmd("tag")
 	rawTags := strings.Split(out, "\n")
-
 	var versions []*semver.Version
 	for _, tag := range rawTags {
-		if verReg.MatchString(tag) {
-			v, _ := semver.NewVersion(tag)
+		t := strings.TrimSpace(tag)
+		if verReg.MatchString(t) {
+			v, _ := semver.NewVersion(t)
 			versions = append(versions, v)
 		}
 	}
@@ -46,4 +47,35 @@ func (gh *Ghch) Versions() []string {
 		vers[i] = v.Original()
 	}
 	return vers
+}
+
+var prMergeReg = regexp.MustCompile(`^[a-f0-9]{7} Merge pull request #([0-9]+) from`)
+
+func (gh *Ghch) MergedPRNums(argv ...string) (nums []string) {
+	var from, to string
+	if len(argv) > 0 {
+		from = argv[0]
+	}
+	if len(argv) > 1 {
+		from = argv[1]
+	}
+	if from == "" {
+		vers := gh.Versions()
+		if len(vers) < 1 {
+			return
+		}
+		from = vers[0]
+	}
+	revisionRange := fmt.Sprintf("%s...%s", from, to)
+	out, err := gh.Cmd("log", revisionRange, "--merges", "--oneline")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(out, "\n")
+	for _, line := range lines {
+		if matches := prMergeReg.FindStringSubmatch(line); len(matches) > 1 {
+			nums = append(nums, matches[1])
+		}
+	}
+	return
 }
